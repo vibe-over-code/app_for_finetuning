f352c45eb38df586b7053bb3e1315b42c7031be2
void-start <81857867+void-start@users.noreply.github.com>
2026-03-01 13:46:44 +0500

    Revert "Update datasets_from_citats.py"
    This reverts commit 299459f1e61553902225695cbf4653b81b9cd3eb.

--- datasetts/datasets_from_citats.py
+++ datasetts/datasets_from_citats.py
@@ -5,9 +5,9 @@ import uuid
 from requests import post
 
 # --- КОНФИГУРАЦИЯ ---
-API_KEY = "" 
+API_KEY = "ftH40nqIasXUPPKYgWINxwdoEWQarNZ0" 
 INPUT_FILE = "dataset1.txt"
-OUTPUT_FILE = "expanded_dataset.jsonl"
+OUTPUT_FILE = "johnny_expanded_dataset.jsonl"
 URL = "https://api.mistral.ai/v1/chat/completions"
 MODEL = "mistral-large-latest"
 
